70e5f6adea59acf52acbb3c480037701f5588339
Huy Pham <pgiahuy653@gmail.com>
2023-06-09 20:04:36 +0700

    fix: book ticket

--- server/src/book-ticket/book-ticket.service.ts
+++ server/src/book-ticket/book-ticket.service.ts
@@ -4,7 +4,7 @@ import { UpdateBookTicketDto } from './dto/update-book-ticket.dto';
 import { Prisma, PrismaClient } from '@prisma/client';
 import { JwtService } from '@nestjs/jwt';
 import { BookTicket, CreateShowtimes } from './entities/book-ticket.entity';
-
+import { v4 as uuidv4 } from 'uuid';
 @Injectable()
 export class BookTicketService {
   constructor(private jwtService: JwtService) {}
@@ -40,31 +40,35 @@ export class BookTicketService {
 
       const showtimes = await this.prisma.showtimes.findFirst({
         where: {
-          showtimes_id 
-        }
+          showtimes_id: Number(showtimes_id),
+        },
       });
 
-
-      
       if (!showtimes) {
         throw new Error('Không tìm thấy lịch chiếu (showtimes)');
       }
 
       const bookTicket_Records = [];
-      // lấy từng vé trong mảng ra 
-      for (const ticket of list_ticket) {
-        const { seat_id, ticket_price } = ticket;
-        const seat = await this.prisma.seat.findFirst({ where: { seat_id } });
+      // lấy từng vé trong mảng ra
+      for (let ticket of list_ticket) {
+        let { seat_id, ticket_price } = ticket;
+        console.log(seat_id, 'seat _ id');
+        let seat = await this.prisma.seat.findFirst({
+          where: { seat_id: Number(seat_id) },
+        });
+        console.log(seat, 'set a');
         if (!seat) {
           throw new Error('Seat not found');
         }
+        // let newId = Math.floor(Math.random() * Date.now()).toString(16);
 
-        const createBook_Ticket = await this.prisma.book_ticket.create({
+        let createBook_Ticket = await this.prisma.book_ticket.create({
           data: {
+            book_ticket_id: uuidv4(),
             users: { connect: { email: infoUser.email } },
-            showtimes: { connect: { showtimes_id } },
-            seat: { connect: { seat_id } },
-            ticket_price: String(ticket_price),
+            showtimes: { connect: { showtimes_id: Number(showtimes_id) } },
+            seat: { connect: { seat_id: Number(seat_id) } },
+            ticket_price: parseInt(ticket_price),
           },
         });
         bookTicket_Records.push(createBook_Ticket);
@@ -75,41 +79,43 @@ export class BookTicketService {
     }
   }
   async createShowTimes(req: Request, body) {
-    const { film_id, showing_times, cinema_id, ticket_price } = body;
+    let { film_id, showing_times, cinema_id, ticket_price } = body;
     const errors = {
-      film_id:'',
-      showing_times:"",
-      cinema_id:"",
-      ticket_price:""
+      film_id: '',
+      showing_times: '',
+      cinema_id: '',
+      ticket_price: '',
     };
 
-    if(!film_id || !showing_times || !cinema_id || !ticket_price){
-      if (!film_id) errors.film_id = "Mã phim không được bỏ trống!!";
-      if (!showing_times) errors.showing_times = "Ngày giờ chiếu không được bỏ trống!";
-      if (!cinema_id) errors.cinema_id = "Mã rạp không được bỏ trống!";
-      if (!ticket_price) errors.ticket_price = "Giá vé không được bỏ trống!";
-      return  { message: 'Fail' , errors };
+    if (!film_id || !showing_times || !cinema_id || !ticket_price) {
+      if (!film_id) errors.film_id = 'Mã phim không được bỏ trống!!';
+      if (!showing_times)
+        errors.showing_times = 'Ngày giờ chiếu không được bỏ trống!';
+      if (!cinema_id) errors.cinema_id = 'Mã rạp không được bỏ trống!';
+      if (!ticket_price) errors.ticket_price = 'Giá vé không được bỏ trống!';
+      return { message: 'Fail', errors };
     }
 
     const film_idExist = await this.prisma.film.findFirst({
-      where:{
-        film_id: parseInt(film_id)
-      }
-    })
-    if(!film_idExist) return { message: 'Fail', error : 'Không tồn tại id film'};
+      where: {
+        film_id: parseInt(film_id),
+      },
+    });
+    if (!film_idExist)
+      return { message: 'Fail', error: 'Không tồn tại id film' };
     const cinema_idExist = await this.prisma.cinema.findFirst({
-      where:{
-        cinema_id:parseInt(cinema_id)
-      }
-    })
-    if(!cinema_idExist) return { message: 'Fail', error : 'Không tồn tại id cinema'};
-
+      where: {
+        cinema_id: parseInt(cinema_id),
+      },
+    });
+    if (!cinema_idExist)
+      return { message: 'Fail', error: 'Không tồn tại id cinema' };
 
     const saved = await this.prisma.showtimes.create({
       data: {
         film: { connect: { film_id: parseInt(film_id) } },
         cinema: { connect: { cinema_id: parseInt(cinema_id) } },
-        showing_times, // Convert to DateTime type 
+        showing_times, // Convert to DateTime type
         ticket_price,
       },
     });
